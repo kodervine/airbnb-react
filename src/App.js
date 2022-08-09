c7e4d5418bb480697a5d092d0817e022aa0f5217
Laskellyb <anikwenzekelly@gmail.com>
2022-08-09 09:45:58 +0100

    More code with more bugs

--- src/App.js
+++ src/App.js
@@ -10,6 +10,30 @@ export default function App() {
       <Navbar />
       <Hero />
       <Card />
+      <Contact 
+          image="./images/mr-whiskerson.png" 
+          name="Mr. Whiskerson"
+          phone="(212) 555-1234"
+          email="mr.whiskaz@catnap.meow"
+            />
+      <Contact 
+        image="./images/fluffykins.png"
+        name="Fluffykins"
+        phone="(212) 555-2345"
+        email="fluff@me.com"
+            />
+      <Contact 
+          image="./images/felix.png"
+          name="Felix"
+          phone="(212) 555-4567"
+          email="thecat@hotmail.com"
+            />
+      <Contact 
+          image="./images/pumpkin.png"
+          name="Pumpkin"
+          phone="(0800) CAT KING"
+          email="pumpkin@scrimba.com"
+            />
     </div> 
   );
 }
